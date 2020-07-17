0ad007bba1eb9423b93fcfebad56ed47680ad225
fumelolz <thefumetero@gmail.com>
2020-07-17 09:42:59 -0500

    Update plantilla.controlador.php

--- controladores/plantilla.controlador.php
+++ controladores/plantilla.controlador.php
@@ -10,8 +10,8 @@ class ControladorPlantilla{
 
 	static public function url(){
 
-		// return "http://35.238.253.36/";
-		return "http://localhost/recetillas/";
+		return "http://35.238.253.36/";
+		// return "http://localhost/recetillas/";
 
 	}
 
